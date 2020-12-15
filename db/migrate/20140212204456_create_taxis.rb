97227264de1996fe08638f776fffd38f8d333c7d
Charlie Knight <williamchknight@gmail.com>
2020-12-15 15:26:28 -0800

    done

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -1,6 +1,7 @@
-class CreateTaxis < ActiveRecord::Migration
+class CreateTaxis < ActiveRecord::Migration[4.2]
   def change
     create_table :taxis do |t|
+      t.string :name
       t.timestamps null: false
     end
   end
--- db/migrate/20140212205318_create_passengers.rb
+++ db/migrate/20140212205318_create_passengers.rb
@@ -1,6 +1,7 @@
-class CreatePassengers < ActiveRecord::Migration
+class CreatePassengers < ActiveRecord::Migration[4.2]
   def change
     create_table :passengers do |t|
+      t.string :name
       t.timestamps null: false
     end
   end
